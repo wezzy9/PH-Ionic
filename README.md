91214126d57aafca1de989f6d3a988dbdc5f31a6
wezzy9 <61868913+wezzy9@users.noreply.github.com>
2024-01-18 17:17:08 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# PH-Ionic
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/wezzy9/PH-Ionic)
---
+++ src/app/home/home-page.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { HomePageComponent } from './home-page.component';
+
+import { IonicModule } from '@ionic/angular';
+
+@NgModule({
+  imports: [IonicModule, RouterModule.forChild([{ path: '', component: HomePageComponent }])],
+  declarations: [HomePageComponent],
+  exports: [HomePageComponent],
+})
+export class HomePageModule {}
